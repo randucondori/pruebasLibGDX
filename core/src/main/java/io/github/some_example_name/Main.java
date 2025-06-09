5fd5284c84335398183c40e5800ca6193d0e0066
angelo <angelo.carp1901@gmail.com>
2025-06-09 11:07:29 +0200

    separar personage de main

--- core/src/main/java/io/github/some_example_name/Main.java
+++ core/src/main/java/io/github/some_example_name/Main.java
@@ -7,149 +7,95 @@ import com.badlogic.gdx.InputAdapter;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.MathUtils;
-import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector3;
 
 import java.util.ArrayList;
 
 public class Main extends ApplicationAdapter {
-    //texturas y utilidades
+
     private OrthographicCamera camera;
     private SpriteBatch batch;
-    private Texture playerTexture;
-    // Jugador
-    private Rectangle jugador;
-    private float velocidad = 200f;
+
+    private Personaje jugador;
     private final float ANCHO_PERSONAJE = 25f;
     private final float ALTO_PERSONAJE = 25f;
+    private final float velocidad = 200f;
 
-    // Mapa
     private Laverinto laverinto;
     private ArrayList<Muro> muros;
     private float mapaAncho = 2000;
     private float mapaAlto = 2000;
-    private Vector3 objetivoCamara; // Usamos// Vector3 que es compatible con camera.position
-    // Control de movimiento
-    private boolean moviendoArriba = false;
-    private boolean moviendoAbajo = false;
-    private boolean moviendoIzquierda = false;
-    private boolean moviendoDerecha = false;
-    //animacion
-    private Animation<TextureRegion> animacion;
-    private TextureRegion jugadorframe;
-    private float tiempo = 0f;
-    //Animaciones de Menu y pantalla de inicio
+    private Vector3 objetivoCamara;
+
     private Texture pantalladDeInicio;
     private Sprite menusprite;
     private float transparencia = 0;
-    //Menu de Inicio
+
     private Texture botonInicio;
     private Texture botonFin;
     private Sprite boton1sprite;
     private Sprite boton2sprite;
 
-
     @Override
     public void create() {
-        // Inicializar vector objetivo (Vector3 para compatibilidad con cámara)
         objetivoCamara = new Vector3();
 
-        // Configurar cámara
         camera = new OrthographicCamera();
         camera.setToOrtho(false, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-        // Cargar texturas
-        playerTexture = new Texture(Gdx.files.internal("imagenes/player.png"));
+
+        batch = new SpriteBatch();
+
+        Texture texturaJugador = new Texture(Gdx.files.internal("imagenes/player.png"));
         pantalladDeInicio = new Texture("mapa/maps.png");
         botonInicio = new Texture("imagenes/puerta.png");
         botonFin = new Texture("imagenes/puerta.png");
         boton1sprite = new Sprite(botonInicio);
         boton2sprite = new Sprite(botonFin);
 
-        // Inicializar jugador
-        jugador = new Rectangle();
-        jugador.width = ANCHO_PERSONAJE;
-        jugador.height = ALTO_PERSONAJE;
-        jugador.x = (mapaAncho - jugador.width) / 2;
-        jugador.y = (mapaAlto - jugador.height) / 2;
+        jugador = new Personaje(
+            (mapaAncho - ANCHO_PERSONAJE) / 2,
+            (mapaAlto - ALTO_PERSONAJE) / 2,
+            ANCHO_PERSONAJE,
+            ALTO_PERSONAJE,
+            velocidad,
+            texturaJugador
+        );
 
-        //iniciar laverinto
         laverinto = new Laverinto();
         muros = laverinto.Muros();
 
-
-        //Iniciar pintador y controles
-        batch = new SpriteBatch();
         configurarControles();
-
-        // Inicializar posición de la cámara
         actualizarCamara();
     }
-    private void crearAnimacion(){
-        TextureRegion[][] tmp = TextureRegion.split(playerTexture, playerTexture.getWidth()/3, playerTexture.getHeight());
-        TextureRegion[] arrayregion = new TextureRegion[3];
-        for (int i = 0; i < 3; i++) arrayregion[i] = tmp[0][i];
-        animacion = new Animation(0.5f, arrayregion);
-    }
-
-    private boolean cambioDeAnimacion(){
-        boolean cambio;
-        if(moviendoArriba){
-            playerTexture=new Texture(Gdx.files.internal("imagenes/puerta.png"));
-            cambio = true;
-        } else if (moviendoAbajo) {
-            playerTexture = new Texture(Gdx.files.internal("imagenes/player.png"));
-            cambio = true;
-        }else if (moviendoIzquierda) {
-            playerTexture=new Texture(Gdx.files.internal("imagenes/puerta.png"));
-            cambio = true;
-        } else if (moviendoDerecha) {
-            playerTexture=new Texture(Gdx.files.internal("imagenes/player.png"));
-            cambio = true;
-        }else{
-            playerTexture=new Texture(Gdx.files.internal("imagenes/puerta.png"));
-            cambio = false;
-        }
-        if (cambio){
-            crearAnimacion();
-        }
-        return cambio;
-    }
-
 
     @Override
     public void render() {
-        // Limpiar pantalla
         Gdx.gl.glClearColor(0.1f, 0.1f, 0.1f, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-        //Animacion de Inico
-        if (pantallaInicio()) {
-            //Despliegue de menu
 
-            // Dibujar
+        if (pantallaInicio()) {
             batch.setProjectionMatrix(camera.combined);
             laverinto.pintarFondo(batch, mapaAncho, mapaAlto);
+
             batch.begin();
-            // Actualizar
-            actualizarMovimiento(Gdx.graphics.getDeltaTime() * 2f);
-            actualizarCamara();
-            if (cambioDeAnimacion()) {
-                tiempo += Gdx.graphics.getDeltaTime();
-                jugadorframe = animacion.getKeyFrame(tiempo, true);
-                batch.draw(jugadorframe, jugador.x, jugador.y, jugador.width, jugador.height);
-            } else batch.draw(playerTexture, jugador.x, jugador.y, jugador.width, jugador.height);
+
+            jugador.actualizarMovimiento(Gdx.graphics.getDeltaTime() * 2f, muros, mapaAncho, mapaAlto);
+            jugador.dibujar(batch, Gdx.graphics.getDeltaTime());
+
             batch.end();
+
             for (Muro muro : muros) {
                 muro.pintarmuro(batch);
             }
+
+            actualizarCamara();
         }
     }
 
-    private boolean pantallaInicio () {
+    private boolean pantallaInicio() {
         menusprite = new Sprite(pantalladDeInicio);
         boolean respuesta = false;
         batch.begin();
@@ -158,37 +104,28 @@ public class Main extends ApplicationAdapter {
             transparencia = 1;
             respuesta = true;
         }
-
         menusprite.setAlpha(transparencia);
         menusprite.draw(batch);
         batch.end();
         return respuesta;
     }
 
-    private boolean menu () {
-        boolean respuesta = false;
-        if (Gdx.input.isTouched()) {
-            //if (Gdx.input.getX()<
-        }
-        return respuesta;
-    }
-
-    private void configurarControles () {
+    private void configurarControles() {
         Gdx.input.setInputProcessor(new InputAdapter() {
             @Override
             public boolean keyDown(int keycode) {
                 switch (keycode) {
                     case Input.Keys.UP:
-                        moviendoArriba = true;
+                        jugador.setMoviendoArriba(true);
                         break;
                     case Input.Keys.DOWN:
-                        moviendoAbajo = true;
+                        jugador.setMoviendoAbajo(true);
                         break;
                     case Input.Keys.LEFT:
-                        moviendoIzquierda = true;
+                        jugador.setMoviendoIzquierda(true);
                         break;
                     case Input.Keys.RIGHT:
-                        moviendoDerecha = true;
+                        jugador.setMoviendoDerecha(true);
                         break;
                 }
                 return true;
@@ -198,90 +135,57 @@ public class Main extends ApplicationAdapter {
             public boolean keyUp(int keycode) {
                 switch (keycode) {
                     case Input.Keys.UP:
-                        moviendoArriba = false;
+                        jugador.setMoviendoArriba(false);
                         break;
                     case Input.Keys.DOWN:
-                        moviendoAbajo = false;
+                        jugador.setMoviendoAbajo(false);
                         break;
                     case Input.Keys.LEFT:
-                        moviendoIzquierda = false;
+                        jugador.setMoviendoIzquierda(false);
                         break;
                     case Input.Keys.RIGHT:
-                        moviendoDerecha = false;
+                        jugador.setMoviendoDerecha(false);
                         break;
                 }
+
                 return true;
             }
         });
     }
 
-    private void actualizarMovimiento ( float delta){
-        float oldx = jugador.x;
-        float oldy = jugador.y;
-
-        if (moviendoArriba) {
-            jugador.y += velocidad * delta;
-        }
-        else if (moviendoAbajo) {
-            jugador.y -= velocidad * delta;
-        }
-        else if (moviendoIzquierda) jugador.x -= velocidad * delta;
-        else if (moviendoDerecha) jugador.x += velocidad * delta;
-        for (Muro m : muros) {
-            if (jugador.overlaps(m.getRectangle())) {
-                jugador.x = oldx;
-                jugador.y = oldy;
-                System.out.println("tocaste un muro");
-            }
-        }
-
-        // Limitar al mapa
-        jugador.x = MathUtils.clamp(jugador.x, 0, mapaAncho - jugador.width);
-        jugador.y = MathUtils.clamp(jugador.y, 0, mapaAlto - jugador.height);
-    }
-
-    private void actualizarCamara () {
-        // Configurar objetivo (centrado en jugador)
+    private void actualizarCamara() {
         objetivoCamara.set(
-            jugador.x + jugador.width / 2,
-            jugador.y + jugador.height / 2,
-            0 // El componente Z siempre es 0 en cámaras 2D
+            jugador.getRect().x + jugador.getRect().width / 2,
+            jugador.getRect().y + jugador.getRect().height / 2,
+            0
         );
 
-        // Interpolación suave (5f = factor de suavizado)
         camera.position.lerp(objetivoCamara, 5f * Gdx.graphics.getDeltaTime());
 
-        // Limitar cámara a bordes del mapa
         float halfViewportWidth = camera.viewportWidth / 2;
         float halfViewportHeight = camera.viewportHeight / 2;
 
-        camera.position.x = MathUtils.clamp(
-            camera.position.x,
-            halfViewportWidth,
-            mapaAncho - halfViewportWidth
-        );
-
-        camera.position.y = MathUtils.clamp(
-            camera.position.y,
-            halfViewportHeight,
-            mapaAlto - halfViewportHeight
-        );
+        camera.position.x = MathUtils.clamp(camera.position.x, halfViewportWidth, mapaAncho - halfViewportWidth);
+        camera.position.y = MathUtils.clamp(camera.position.y, halfViewportHeight, mapaAlto - halfViewportHeight);
 
         camera.update();
     }
 
     @Override
-    public void dispose () {
+    public void dispose() {
         batch.dispose();
-        playerTexture.dispose();
+        jugador.dispose();
         laverinto.dispoce();
+        pantalladDeInicio.dispose();
+        botonInicio.dispose();
+        botonFin.dispose();
     }
 
     @Override
-    public void resize ( int width, int height){
+    public void resize(int width, int height) {
         camera.viewportWidth = width;
         camera.viewportHeight = height;
         camera.update();
     }
-
 }
+
---
+++ core/src/main/java/io/github/some_example_name/Personaje.java
@@ -0,0 +1,128 @@
+package io.github.some_example_name;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.utils.Disposable;
+
+public class Personaje implements Disposable {
+    private Rectangle rect;
+    private float velocidad;
+    private Texture playerTexture;
+    private Animation<TextureRegion> animacion;
+    private TextureRegion jugadorframe;
+    private float tiempo;
+    private boolean moviendoArriba, moviendoAbajo, moviendoIzquierda, moviendoDerecha;
+
+    private final float ancho;
+    private final float alto;
+
+    public Personaje(float x, float y, float ancho, float alto, float velocidad, Texture texturaInicial) {
+        this.ancho = ancho;
+        this.alto = alto;
+        this.velocidad = velocidad;
+        this.playerTexture = texturaInicial;
+
+        rect = new Rectangle(x, y, ancho, alto);
+        tiempo = 0f;
+        crearAnimacion();
+
+        moviendoArriba = moviendoAbajo = moviendoIzquierda = moviendoDerecha = false;
+
+    }
+
+    private void crearAnimacion() {
+        int columnas = 3;
+        int filas = 1;
+        
+        TextureRegion[][] regiones = TextureRegion.split(
+            playerTexture,
+            playerTexture.getWidth() / columnas,
+            playerTexture.getHeight() / filas
+        );
+
+        if (regiones.length < 1 || regiones[0].length < columnas) {
+            throw new IllegalArgumentException("ups¡");
+        }
+
+        TextureRegion[] frames = new TextureRegion[columnas];
+        for (int i = 0; i < columnas; i++) {
+            frames[i] = regiones[0][i];
+        }
+
+        animacion = new Animation<>(0.5f, frames);
+    }
+
+
+    public void actualizarMovimiento(float delta, Iterable<Muro> muros, float mapaAncho, float mapaAlto) {
+        float oldX = rect.x;
+        float oldY = rect.y;
+
+        if (moviendoArriba) rect.y += velocidad * delta;
+        if (moviendoAbajo) rect.y -= velocidad * delta;
+        if (moviendoIzquierda) rect.x -= velocidad * delta;
+        if (moviendoDerecha) rect.x += velocidad * delta;
+
+        for (Muro m : muros) {
+            if (rect.overlaps(m.getRectangle())) {
+                rect.x = oldX;
+                rect.y = oldY;
+                System.out.println("tocaste un muro");
+                break;
+            }
+        }
+
+        // Limitar a mapa
+        rect.x = MathUtils.clamp(rect.x, 0, mapaAncho - rect.width);
+        rect.y = MathUtils.clamp(rect.y, 0, mapaAlto - rect.height);
+    }
+
+    public void dibujar(com.badlogic.gdx.graphics.g2d.SpriteBatch batch, float delta) {
+        if (hayMovimiento()) {
+            tiempo += delta;
+            jugadorframe = animacion.getKeyFrame(tiempo, true);
+            batch.draw(jugadorframe, rect.x, rect.y, rect.width, rect.height);
+        } else {
+            batch.draw(playerTexture, rect.x, rect.y, rect.width, rect.height);
+        }
+    }
+
+    public boolean hayMovimiento() {
+        return moviendoArriba || moviendoAbajo || moviendoIzquierda || moviendoDerecha;
+    }
+
+    public void setMovimiento(boolean arriba, boolean abajo, boolean izquierda, boolean derecha) {
+        this.moviendoArriba = arriba;
+        this.moviendoAbajo = abajo;
+        this.moviendoIzquierda = izquierda;
+        this.moviendoDerecha = derecha;
+    }
+
+    public Rectangle getRect() {
+        return rect;
+    }
+    public void setMoviendoArriba(boolean estado) {
+        this.moviendoArriba = estado;
+    }
+
+    public void setMoviendoAbajo(boolean estado) {
+        this.moviendoAbajo = estado;
+    }
+
+    public void setMoviendoIzquierda(boolean estado) {
+        this.moviendoIzquierda = estado;
+    }
+
+    public void setMoviendoDerecha(boolean estado) {
+        this.moviendoDerecha = estado;
+    }
+
+
+    @Override
+    public void dispose() {
+        playerTexture.dispose();
+    }
+}
+
